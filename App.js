5ac3861b502cc11c9273c8e90bc3b2df4f67cc4b
Dev Raj <devcuber45@gmail.com>
2025-06-24 22:27:32 +0530

    +json and jsx example

--- App.js
+++ App.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-const parent=React.createElement('div', {id:"parent"}, 
-            React.createElement('div', {id:"child"},
-            React.createElement('h1', {id:"h1"}, "I am a H1 heading"),
-            // React.createElement('p', {id:"para"}, "This is a paragraph")
-        ));
-        const root = ReactDOM.createRoot(document.getElementById('root'));
-        root.render(parent);
+
+const Heading = () => {
+    return <h1 id="heading"> I am a Heading component </h1>};
+        const root = ReactDOM.createRoot(document.getElementById("root"));
+        // const jsxHeading = <h1 id="heading"> I am a boy toy </h1>; 
+        root.render(<Heading/>);
